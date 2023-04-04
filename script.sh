69b8ce4f3d4184d589a2c6e32ab82ff30fb29873
Bastien Guillaume <bguillau@student.42.fr>
2023-04-04 19:32:12 +0200

    interactive ok, error correction

--- script.sh
+++ script.sh
@@ -1,36 +1,33 @@
 #!/bin/zsh
+source utils.sh
 
 # help/usage mode
-[[ $1 =~ "-h|-help|-usage|--help|--usage" ]] && echo "\033[1mPRINT USAGE\033[0m" && exit 0
-# includes (only needed for interactive mode)
-[[ $1 == "-i" ]] && source search_stop.sh
+[[ $1 =~ "^(-i|-h|-help|-usage|--help|--usage)$" ]] && echo "\033[1mPRINT USAGE\033[0m" && exit 0
 
 # token infos
 token_path=~/.token_idfm
 token=$(cat $token_path 2>/dev/null)
-[[ $(echo $token | wc -c) -gt 1 ]] || {echo "token empty" && exit 45}
+[[ $(echo $token | wc -c) -gt 1 ]] || {echo "token empty" && exit 43}
 
 # research infos (mandatory for non interactive use)
 line="C01743"
-stop="474151"
-terminus=("mitry")
+stop="45102"
+terminus=("robinson" "mitry" "charles")
 
 # interactive mode (reset line stop)
-[[ $1 == "-i" ]] && echo "\033[1;4mInteractive mode\033[0m" && get_stop && get_line
+[[ $1 == "-i" ]] && {echo "\033[1;4;30;47mINTERACTIVE MODE\033[0m" ; get_stop ; get_line}
+
+# unwanted argument
+[[ $# -ge 1 ]] && { [[ $1 =~ "^(-i|-h|-help|-usage|--help|--usage)$" ]] || echo "Error : argument \"$1\" unrecognized" && exit 44 }
+
 
 # requete api idfm
 header="apikey: $token"
 url="https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?\
 MonitoringRef=STIF%3AStopArea%3ASP%3A$stop%3A&LineRef=STIF%3ALine%3A%3A$line%3A"
 req=$(curl -s -i -X 'GET' -H $header $url)
-#echo $req
+#echo $req | tr "," "\n"
 
-# Error msg
-token_err="Error 401 : Invalid authentication credentials (bad token for Idfm API)"
-i_e="Input error :"
-sl_err="$i_e Stop code $stop is not a stop of line $line"
-stop_err="$i_e Stop code $stop is not recognized"
-line_err="$i_e Line code $line is not recognized"
 
 # check code (401 token, 200 ok, 500||503 api ko, 400 input err)
 code=$(echo $req | grep -m1 'HTTP' | egrep -o '[0-9]{3}')
@@ -38,50 +35,41 @@ echo $code | egrep -q '401' && echo $token_err && exit 45
 echo $code | egrep -q '500|503' && echo "IDFM API KO" && exit 46
 err=$(echo $code | egrep -q '400' && echo $req | grep -o "\"ErrorText\":\".*\"," | cut -d':' -f2 | tr -d "\",")
 [[ `echo $err | wc -c` -gt 0 ]] && echo $err | grep -q "MonitoringRef/LineRef" && echo $sl_err && exit 47
-[[ `echo $err | wc -c` -gt 0 ]] && echo $err | grep -q "MonitoringRef" && echo $stop_err && exit 47
-[[ `echo $err | wc -c` -gt 0 ]] && echo $err | grep -q "LineRef" && echo $line_err && exit 48
+[[ `echo $err | wc -c` -gt 0 ]] && echo $err | grep -q "MonitoringRef" && echo $stop_err && exit 48
+[[ `echo $err | wc -c` -gt 0 ]] && echo $err | grep -q "LineRef" && echo $line_err && exit 49
 
 # Get terminuses from req (interactive mode)
-test=()
-test=$(echo $req | tr "," "\n" | grep -i destinationname | sort | uniq | egrep -o "\"value\":\".*}]$" | cut -d':' -f2 | egrep -o "\".*\"" | tr -d "\"" | tr "ÄäÂâÀà" "a" |tr "ÈèÉéËëÊê" "e" | tr "ÏïÎî" "i" | tr "ÖöÔô" "o" | tr "ÜüÛû" "u" | tr "Çç" "c" | tr "[A-Z]" "[a-z]")
-echo $test[@]
-#exit 42
+[[ $1 == "-i" ]] && get_terminus
 
-#possible_terminus=$(   )
-
-for term in "${terminus[@]}" ;
-do
-	next_times+=$(echo $req | tr "{" "\n" | grep -i $term | tr "," "\n" | grep -i departuretime | egrep -o "[0-9]{2}:[0-9]{2}")$(echo " ")
-done
-
-# set France GMT offset (+1 winter, +2 summer)
-date | egrep -q 'CEST|CET' || {echo "Problem with date format : (CET or CEST expected)" && exit 49}
+# time mgmt : paris gmt + relative time
+date | egrep -q 'CEST|CET' || {echo "Problem with date format : (CET or CEST expected)" && exit 50}
 date | grep -q CET && GMT=1 || GMT=2
-# if unable to get CET or CEST in date cmd, provide the current Paris GMT offset below
+# not CET or CEST in date cmd? delete both upper lines and provide Paris GMT offset below
 #GMT=x
-
-# for relative time
 current_time=$(date +%R)
 ct_hours=$(echo $current_time | cut -d':' -f1)
 ct_min=$(echo $current_time | cut -d':' -f2)
 
-RES=($(echo $next_times))
+# print intro
+retrieve_codenline_names
+echo "\033[1;4;44mNext $line_name at $stop_name:\033[m"
 
-#print result intro
-echo "Next $line at stop $stop direction $terminus:"
-
-# GMT correction + relative time calculation + prints out results
-for res in "${RES[@]}"; 
+# print results
+for term in "${terminus[@]}" ;
 do
-	min=$(echo $res | cut -d':' -f2)
-	hours=$(( $(echo $res | cut -d':' -f1) + $GMT ))
-	[[ $hours -gt 24 ]] && hours=$(($hours - 24))
-	res=$(echo $hours`echo :``echo $res | cut -d':' -f2`)
-	delta=$(( (($hours - $ct_hours) * 60) + $min - $ct_min ))
-	[[ $delta -lt 0 ]] && echo "next at $res (retard)" || echo "next at $res (in $delta min)"
+	echo "\033[4mTerminus: $(echo $term | tr "_" " ")\033[m"
+	next_times=($(echo $req | tr "{" "\n" | iconv -f UTF-8 -t ASCII//TRANSLIT | tr "[A-Z]" "[a-z]" | grep -i "$(echo $term | iconv -f UTF-8 -t ASCII//TRANSLIT | tr "[A-Z]" "[a-z]" | tr "_" " ")" | tr "," "\n" | grep -i departuretime | egrep -o "[0-9]{2}:[0-9]{2}")$(echo " "))
+	for time in "${next_times[@]}" ;
+	do
+		min=$(echo $time | cut -d':' -f2)
+		hours=$(( $(echo $time | cut -d':' -f1) + $GMT ))
+		[[ $hours -gt 24 ]] && hours=$(($hours - 24))
+		time=$(echo $hours`echo :``echo $time | cut -d':' -f2`)
+		delta=$(( (($hours - $ct_hours) * 60) + $min - $ct_min ))
+		[[ $delta -lt 0 ]] && echo "\t$time (retard)" || echo "\t$time (in $delta min)"
+	done
 done
 
-# - afficher terminus (le recup somehow dans next_times)
-# - erreur de terminus
-# - recup les lignes (rer tram metro) + mode interactif
-
+# pb si departure stop == terminus (faudrait quitter) mais bon ...
+# print usage
+# readme propre
---
+++ utils.sh
@@ -0,0 +1,71 @@
+# Error msg
+token_err="Error 401 : Invalid authentication credentials (bad token for Idfm API)"
+i_e="Input error :"
+sl_err="$i_e Stop code $stop is not a stop of line $line"
+stop_err="$i_e Stop code $stop is not recognized"
+line_err="$i_e Line code $line is not recognized"
+
+# files
+line_file=referentiel-des-lignes.csv
+[[ -r $line_file ]] || {echo "File $line_file does not exist or is not readable" && exit 6}
+line_clean=$(cat $line_file | sed s'/;/ (/' | sed s'/;/ /' | sed s'/$/)/g' | sed s'/^/--> /g' | sort)
+
+stop_file=zones-d-arrets.csv
+[[ -r $stop_file ]] || {echo "File $stop_file does not exist or is not readable" && exit 5}
+stop_clean=$(cat $stop_file | sed s'/;/ (/' |  sed s'/;/ ; /g' | sed s'/.$/)/g' | sed s'/^/--> /g')
+
+# function
+function get_stop() 
+{
+	echo -n "\033[1mDeparture stop \033[3m(code or name)\033[m: "
+	read input
+	input=$(echo $input | iconv -f UTF-8 -t ASCII//TRANSLIT | tr "[A-Z]" "[a-z]" | tr -s "[:space:]")
+	res=$(echo $stop_clean | grep "$input")
+	[[ $(echo $res | wc -l) -eq 1 && $(echo $res | wc -c) -gt 2 ]] && stop=`echo $res | cut -d' ' -f2` && echo "\033[2mStop selected: $res\033[m" && return
+	[[ $(echo $res | wc -c) -eq 1 ]] && echo "No entry for $input (CTRL+C to quit)" && get_stop
+	[[ $(echo $res | wc -l) -gt 1 ]] && echo "\n\033[1mMultiple entries found for \"$input\" (be more precise):\033[m\n$(echo $res)\n" && get_stop
+} 
+
+function get_line() 
+{
+	echo -n "\033[1mLine \033[3m(ie. metro 3, rer a, tram t3a; or code)\033[m: "
+	read input
+	input=$(echo $input | iconv -f UTF-8 -t ASCII//TRANSLIT | tr "[A-Z]" "[a-z]" | tr -s "[:space:]")
+	res=$(echo $line_clean | grep -i "$input")
+	[[ $(echo $res | wc -l) -eq 1 && $(echo $res | wc -c) -gt 2 ]] && line=`echo $res | cut -d' ' -f2` && echo "\033[2mLine selected: $res\033[m" && return
+	[[ $(echo $res | wc -c) -eq 1 ]] && echo "No entry for $input (CTRL+C to quit)" && get_line
+	[[ $(echo $res | wc -l) -gt 1 ]] && echo "\n\033[1mMultiple entries found for \"$input\" (be more precise):\033[m\n$(echo $res)\n" && get_line
+}
+
+function get_terminus()
+{
+	test=($(echo $req | tr "," "\n" | grep -i destinationname | sort | uniq | egrep -o "\"value\":\".*}]$" | cut -d':' -f2 | egrep -o "\".*\"" | tr -d "\"" | tr " " "_"))
+	i=0
+	echo "\033[1mAvailable terminuses:\033[0m"
+	for term in "${test[@]}" ;
+	do
+		i=$(($i+1))
+		echo "$i - $(echo $term | tr "_" " ")"
+	done
+	echo -n "\033[1mSelect terminuses via digit (ie. 1 2 4):\033[0m "
+	read selected_term
+	while echo "$selected_term" | egrep -q "[^0-9 ]" && echo -n "Only numbers and spaces are accepted: " ;
+	do
+		read selected_term
+	done
+	echo "\033[2mTerminus selected: $selected_term\033[m"
+	selected_term=($(echo $selected_term))
+	terminus=()
+	for term in "${selected_term[@]}" ;
+	do
+		terminus+=$(echo $test[$term] )
+	done
+}
+
+function retrieve_codenline_names()
+{
+	line_name=$(cat $line_file | grep $line )
+	echo $line_name | grep -q metro && line_name="METRO $(echo $line_name | cut -d';' -f3)" || line_name=$(echo $line_name | cut -d';' -f3)
+	stop_name=$(cat $stop_file | grep $stop | tr "[a-z]" "[A-Z]")
+	stop_name=$(echo $stop_name | cut -d';' -f2)
+}
